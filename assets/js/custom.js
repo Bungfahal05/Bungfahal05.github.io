f5e050ccb6d65e40e37c4b59c9df04703a738134
Bungfahal <116727615+Bungfahal05@users.noreply.github.com>
2023-05-27 20:20:04 +0700

    commit

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+//   Typed
+var typed = new Typed('.typed', {
+    // Waits 1000ms after typing "First"
+    strings: ['Web Developer.', 'Blogger.', 'UI / UX Designer.'],
+    loop: true,
+    typeSpeed: 200,
+    backSpeed: 150
+  });
